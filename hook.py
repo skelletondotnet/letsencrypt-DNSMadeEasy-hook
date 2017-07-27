b60713c09153f3f397264f6fb4bb0d3407fcb8da
skelletondotnet <30506076+skelletondotnet@users.noreply.github.com>
2017-07-27 19:39:01 +0200

    decode rdata.strings from byte type
    The script broke after the update from Debian 8 to Debian 9. (with python3-dnspython 1.25.0-1)  
    rdata.strings returned byte type strings which did not match the token due to different encoding.

--- hook.py
+++ hook.py
@@ -71,7 +71,7 @@ def _has_dns_propagated(name, token):
             dns_response = dns.resolver.query(name, 'TXT')
         for rdata in dns_response:
             for txt_record in rdata.strings:
-                txt_records.append(txt_record)
+                txt_records.append(txt_record.decode())
     except dns.exception.DNSException as error:
         return False
 
